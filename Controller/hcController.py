29ad44a90dbc51d1c18934cdbb7ab0331e755f50
tuanldchainos <tuanldhaihaua@gmail.com>
2021-06-05 14:15:19 +0700

    update main func: Runforever for HcController

--- Controller/hcController.py
+++ Controller/hcController.py
@@ -2,7 +2,7 @@ from BaseServices.httpServices import HttpServices
 from BaseServices.signalrServices import SignalrClient
 from ExtraServices.hcRequestServices import HcRequestServices
 from BaseServices.mqttServices import MqttServices
-
+import asyncio
 
 class HcController:
 
@@ -11,7 +11,25 @@ class HcController:
         self.signalServices = SignalrClient()
         self.hcRequestServices = HcRequestServices()
         self.mqttServices = MqttServices()
+    
+    async def __other_coroutine(self):
+        while True:
+            await asyncio.sleep(1)
+            print("other route")
+
         
-    def RunForever(self):
-        while(1):
-            return
+    async def RunForever(self):
+        self = HcController()
+        self.mqttServices.MqttConnect()
+        self.mqttServices.MqttStartLoop()
+
+        self.signalServices.ConnectToServer()
+        self.signalServices.Start()
+        self.signalServices.OnReceiveData()
+
+        task1 = asyncio.ensure_future(self.__other_coroutine())
+        task2 = asyncio.ensure_future(self.mqttServices.MqttHandlerData())
+        task3 = asyncio.ensure_future(self.signalServices.OnHandlerReceiveData())
+        tasks = [task1, task2, task3]
+
+        await asyncio.gather(*tasks)
