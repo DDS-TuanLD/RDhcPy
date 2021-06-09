901719c5d0d96568af92ca764a72150863d08a47
tuanldchainos <tuanldhaihaua@gmail.com>
2021-06-09 08:14:46 +0700

    eliminate cookie, update getter/setter

--- BaseServices/httpServices.py
+++ BaseServices/httpServices.py
@@ -8,46 +8,45 @@ import os
 
 class HttpRequest():
     __header: CaseInsensitiveDict
-    __cookie: dict
     __body: dict
     __url: str
     
-    def SetHeader(self, header: CaseInsensitiveDict):
+    @property
+    def Body(self):
+        return self.__body
+    
+    @property
+    def Header(self):
+        return self.__header
+    
+    @property 
+    def Url(self):
+        return self.__url
+        
+    @Header.setter
+    def Header(self, header: CaseInsensitiveDict):
         self.__header = header
         return self
     
-    def SetCookie(self, cookie: dict):
-        self.__cookie = cookie
-        return self
-    
-    def SetBody(self, body: dict):
+    @Body.setter
+    def Body(self, body: dict):
         self.__body = body
         return self
     
-    def SetUrl(self, url: str):
+    @Url.setter
+    def Url(self, url: str):
         self.__url = url
         return self
     
-    def GetHeader(self):
-        return self.__header
-    
-    def GetCookie(self):
-        return self.__cookie
-        
-    def GetBody(self):
-        return self.__body
-    
-    def GetUrl(self):
-        return self.__url    
+  
 class HttpServices():
 
     def CreateNewHttpRequest(
-        self, url: str = None, cookie: dict = {}, token: str = "", body_data: dict = {}):
+        self, url: str = None, token: str = "", body_data: dict = {}):
         """ Create new http request
 
         Args:
             url (str): [url want to request]
-            cookie (dict): [cookie of request]
             token (str): [jwt token of request]
             body_data (dict): [body of request]
 
@@ -59,7 +58,10 @@ class HttpServices():
         newHttpHeader["Authorization"] = "Bearer " + token
         
         newHttpRequest = HttpRequest()
-        newHttpRequest.SetBody(body_data).SetCookie(cookie).SetHeader(newHttpHeader).SetUrl(url)
+        newHttpRequest.Body = body_data
+        newHttpRequest.Header = newHttpHeader
+        newHttpRequest.Url = url
+        
         return newHttpRequest
 
     async def UseGetRequest(
@@ -75,7 +77,7 @@ class HttpServices():
         """
         resp = None
         try:
-            async with session.get(req.GetUrl(), cookies=req.GetCookie(), headers=req.GetHeader(), data=req.GetBody()) as resp:
+            async with session.get(req.Url, headers=req.Header, data=req.Body) as resp:
                 resp.raise_for_status()
                 await resp.json()
         except HTTPError as err:  
@@ -97,7 +99,7 @@ class HttpServices():
         """
         resp = None
         try:
-            async with session.post(req.GetUrl(), cookies=req.GetCookie(), headers=req.GetHeader(), data=req.GetBody()) as resp:
+            async with session.post(req.Url, headers=req.Header, data=req.Body) as resp:
                 resp.raise_for_status()
                 await resp.json()
         except HTTPError as err:  
@@ -119,7 +121,7 @@ class HttpServices():
         """
         resp = None
         try:
-            async with session.put(req.GetUrl(), cookies=req.GetCookie(), headers=req.GetHeader(), data=req.GetBody()) as resp:
+            async with session.put(req.Url, headers=req.Header, data=req.Body) as resp:
                 resp.raise_for_status()
                 await resp.json()
         except HTTPError as err:  
@@ -141,7 +143,7 @@ class HttpServices():
         """
         resp = None
         try:
-            async with session.delete(req.GetUrl(), cookies=req.GetCookie(), headers=req.GetHeader(), data=req.GetBody()) as resp:
+            async with session.delete(req.Url, headers=req.Header, data=req.Body) as resp:
                 resp.raise_for_status()
                 await resp.json()
         except HTTPError as err:  
