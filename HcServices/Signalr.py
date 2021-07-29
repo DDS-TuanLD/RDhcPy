4e8c16c58ea6720578aa05b66a4656d83cad2b27
tuanldchainos <49343179+tuanldchainos@users.noreply.github.com>
2021-07-29 22:26:01 +0700

    update write log when dis/connect to signalr

--- HcServices/Signalr.py
+++ HcServices/Signalr.py
@@ -67,11 +67,13 @@ class Signalr(Itransport):
     
     def __disconnect(self):
         print("disconnect to signalr server")
+        self.__logger.debug("disconnect to signalr server")
         self.__disconnectFlag = 0
         self.__disconnectRetryCount = 0
     
     def __onConnect(self):
         self.__hub.on_open(lambda: print("Connect to signalr server"))
+        self.__logger.debug("Connect to signalr server")
         
     def __dataPreHandler(self, data):
         with self.__lock:
